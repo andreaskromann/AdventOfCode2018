b87512bcdd2f7ebd5813f393b52cc203c7c242eb
Andreas Kromann <kromann.andreas@gmail.com>
2018-12-03 19:47:16 +0100

    Infrastructure to run solutions

---
+++ ICodingProblem.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode2018
+{
+    internal interface ICodingProblem
+    {
+        void Run();
+    }
+}
---
+++ Problems/Day1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AdventOfCode2018.Problems
+{
+    class Day1 : ICodingProblem
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
