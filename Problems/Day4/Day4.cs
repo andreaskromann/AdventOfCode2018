dc7ba7dca32d4d150ab2c5949395fe9462a7a8a9
Andreas Kromann <kromann.andreas@gmail.com>
2018-12-04 08:16:55 +0100

    Day 4 part 1

---
+++ Problems/Day4/Day4.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018.Problems.Day4
+{
+    public class Day4 : ICodingProblem
+    {
+        public void Run()
+        {
+            var sleepMap = new Dictionary<int, Dictionary<int, int>>();
+            var currentGuardId = 0;
+            var startSleep = 0;
+            
+            void ParseInput(string s)
+            {
+                var match = Regex.Match(s, @"\[(?<time>[\d\s-:]+)\] (?<action>Guard|falls|wakes) (#(?<id>\d+))?", RegexOptions.Compiled);
+                var minute = DateTimeOffset.Parse(match.Groups["time"].Value).Minute;
+                var action = match.Groups["action"].Value;
+
+                switch (action)
+                {
+                        case "Guard":
+                            currentGuardId = int.Parse(match.Groups["id"].Value);
+                            break;
+                        case "falls":
+                            startSleep = minute;
+                            break;
+                        case "wakes":
+                            if (!sleepMap.ContainsKey(currentGuardId))
+                                sleepMap[currentGuardId] = new Dictionary<int, int>();
+                            for (var i = startSleep; i < minute; i++)
+                            {
+                                if (!sleepMap[currentGuardId].ContainsKey(i))
+                                    sleepMap[currentGuardId][i] = 0;
+                                sleepMap[currentGuardId][i] += 1;
+                            }
+
+                            break;
+                }
+            }
+
+            var data = File.ReadAllLines("Problems\\Day4\\Day4.data");
+            Array.Sort(data);
+            foreach (var line in data)
+                ParseInput(line);
+
+            var maxSleep = (Id : 0, TotalSleep: 0, MaxMinute: 0);
+            foreach (var sleep in sleepMap)
+            {
+                var temp = sleep.Value.Sum(x => x.Value);
+                if (temp > maxSleep.TotalSleep)
+                {
+                    var maxMinute = (Minute: -1, Max:0);
+                    foreach (var kv in sleep.Value)
+                    {
+                        if (kv.Value > maxMinute.Max)
+                            maxMinute = (kv.Key, kv.Value);
+                    }
+                    maxSleep = (sleep.Key, temp, maxMinute.Minute);
+                }
+            }
+            
+            Console.WriteLine($"Part 1: {maxSleep.Id * maxSleep.MaxMinute}");
+        }
+    }
+}
